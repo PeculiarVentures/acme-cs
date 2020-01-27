2033ac17ee782dbb960f4374f2676a37d6ddb63e
microshine <microshine@mail.ru>
2020-01-28 00:47:57 +0300

    Protocol: Add Directory object

---
+++ src/ACME.Core/Protocol/Directory.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace PeculiarVentures.ACME.Protocol
+{
+    /// <summary>
+    /// JSON ACME Directory
+    /// </summary>
+    /// <example>
+    /// {
+    ///   "newNonce": "https://example.com/acme/new-nonce",
+    ///   "newAccount": "https://example.com/acme/new-account",
+    ///   "newOrder": "https://example.com/acme/new-order",
+    ///   "newAuthz": "https://example.com/acme/new-authz",
+    ///   "revokeCert": "https://example.com/acme/revoke-cert",
+    ///   "keyChange": "https://example.com/acme/key-change",
+    ///   "meta": {
+    ///     "termsOfService": "https://example.com/acme/terms/2017-5-30",
+    ///     "website": "https://www.example.com/",
+    ///     "caaIdentities": ["example.com"],
+    ///     "externalAccountRequired": false
+    ///   }
+    /// }
+    /// </example>
+    public class Directory : BaseObject
+    {
+        /// <summary>
+        /// New nonce.
+        /// </summary>
+        [JsonProperty("newNonce")]
+        public string NewNonce { get; set; }
+
+        /// <summary>
+        /// New account.
+        /// </summary>
+        [JsonProperty("newAccount")]
+        public string NewAccount { get; set; }
+
+        /// <summary>
+        /// New order.
+        /// </summary>
+        [JsonProperty("newOrder")]
+        public string NewOrder { get; set; }
+
+        /// <summary>
+        /// New authorization
+        /// </summary>
+        [JsonProperty("newAuthz")]
+        public string NewAuthorization { get; set; }
+
+        /// <summary>
+        /// Revoke certificate
+        /// </summary>
+        [JsonProperty("revokeCert")]
+        public string RevokeCertificate { get; set; }
+
+        /// <summary>
+        /// Key change
+        /// </summary>
+        [JsonProperty("keyChange")]
+        public string KeyChange { get; set; }
+
+        /// <summary>
+        /// Metadata object
+        /// </summary>
+        /// meta (optional, object)
+        [JsonProperty("meta")]
+        public DirectoryMetadata Meta { get; set; }
+
+    }
+}
---
+++ src/ACME.Core/Protocol/DirectoryMetadata.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace PeculiarVentures.ACME.Protocol
+{
+    public class DirectoryMetadata : BaseObject
+    {
+        /// <summary>
+        /// A URL identifying the current terms of service
+        /// </summary>
+        /// termsOfService (optional, string)
+        [JsonProperty("termsOfService")]
+        public string TermsOfService { get; set; }
+
+        /// <summary>
+        /// An HTTP or HTTPS URL locating a website providing more information
+        /// about the ACME server
+        /// </summary>
+        /// website (optional, string)
+        [JsonProperty("website")]
+        public string Website { get; set; }
+
+        /// <summary>
+        /// The hostnames that the ACME server recognizes as referring to itself
+        /// for the purposes of CAA record validation
+        /// </summary>
+        /// <remarks>
+        /// Each string MUST represent the same sequence of ASCII code points
+        /// that the server will expect to see as the "Issuer Domain Name"
+        /// in a CAA issue or issuewild property tag.This allows clients
+        /// to determine the correct issuer domain name to use
+        /// when configuring CAA records
+        /// </remarks>
+        /// caaIdentities (optional, array of string)
+        [JsonProperty("caaIdentities")]
+        public string[] CaaIdentities { get; set; }
+
+        /// <summary>
+        /// If this field is present and set to "true", then the CA requires
+        /// that all newAccount requests include an "externalAccountBinding"
+        /// field associating the new account with an external account
+        /// </summary>
+        /// externalAccountRequired (optional, boolean)
+        [JsonProperty("externalAccountRequired")]
+        public bool? ExternalAccountRequired { get; set; }
+    }
+}
