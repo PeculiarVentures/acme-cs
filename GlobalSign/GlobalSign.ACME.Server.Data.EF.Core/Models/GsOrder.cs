f1f40411977e1c9f58a8ece9c5abe689574a7473
Romashine <miroshin.ry@gmail.com>
2020-04-17 19:04:16 +0300

    Deleted GlobalSign projects

--- GlobalSign/GlobalSign.ACME.Server.Data.EF.Core/Models/GsOrder.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace GlobalSign.ACME.Server.Data.EF.Core.Models
-{
-    public class GsOrder : PeculiarVentures.ACME.Server.Data.EF.Core.Models.Order
-    {
-        public string TemplateId { get; set; }
-    }
-}
