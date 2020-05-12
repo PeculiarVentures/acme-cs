8d42b33e7f5a872dd10e5c03d8db9ab6f0349a28
microshine <microshine@mail.ru>
2020-05-12 03:01:32 +0300

    feat(core): Add UnsupportedContactException

---
+++ src/ACME.Core/Exceptions/UnsupportedContactException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace PeculiarVentures.ACME.Exceptions
+{
+    public class UnsupportedContactException : AcmeException
+    {
+        public UnsupportedContactException()
+            : this("Unsupported contact")
+        {
+        }
+
+        public UnsupportedContactException(string message)
+            : base(Protocol.ErrorType.UnsupportedContact, message, System.Net.HttpStatusCode.BadRequest)
+        { }
+    }
+}
